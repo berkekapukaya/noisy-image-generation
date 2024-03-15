23a1dd8fa29263c9fd2c7b2918a66223ff3b691e
berkekapukaya <73217005+berkekapukaya@users.noreply.github.com>
2024-03-15 11:47:15 +0300

    noisy-and-foggy-image
    noisty and foggy image generation attempted results

---
+++ img2img.py
@@ -0,0 +1,43 @@
+import torch
+from diffusers import AutoPipelineForImage2Image
+from diffusers.utils import load_image
+import random
+
+pipeline = AutoPipelineForImage2Image.from_pretrained(
+    "runwayml/stable-diffusion-v1-5", torch_dtype=torch.float32, variant="fp16", use_safetensors=True
+)
+
+pipeline = pipeline.to("mps")
+
+pipeline.enable_attention_slicing()
+
+# prepare image noisy
+"""
+url ="https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/diffusers/cat.png"
+init_image = load_image('balloons_noisy.png')
+
+prompt = "an image of a couple on a bridge, noisy, colored, photorealistic, noisy image"
+"""
+
+# image foggy
+url = "https://www.ignatianspirituality.com/wp-content/uploads/2015/06/man-in-fog2.jpg"
+init_image = load_image(url)
+
+
+prompt = "a person in fog, fog on the foreground, photorealistic, 8k"
+prompt_alt = "an image of a foggy forest, fog on the foreground, photorealistic, 8k"
+negative_prompt = "bad anatomy, disfigured, ugly, deformed, poor details, disfigured face"
+
+i = 0
+
+while i < 1:
+
+    randSeed = random.randint(1, 1000)
+
+    generator = torch.Generator(device="mps").manual_seed(randSeed)
+
+    # pass prompt and image to pipeline
+    image = pipeline(prompt=prompt_alt, negative_prompt=negative_prompt, generator=generator, image=init_image).images[0]
+    image.save("img2img/img2img%s.png" % randSeed)
+
+    i += 1
---
+++ text2img.py
@@ -0,0 +1,67 @@
+from diffusers import AutoPipelineForText2Image
+import torch
+import random
+
+pipeline = AutoPipelineForText2Image.from_pretrained(
+    "runwayml/stable-diffusion-v1-5",
+    variant="fp16")
+
+pipeline = pipeline.to("mps")
+
+pipeline.enable_attention_slicing()
+
+i = 0
+
+while i < 15:
+
+    randSeed = random.randint(1, 1000)
+
+    generator = torch.Generator(device="mps").manual_seed(randSeed)
+
+   ##30 inference steps blurry
+
+    image = pipeline(
+        prompt="Astronaut in a jungle, cold color palette, muted colors, noisy, blurry, loss of sharpness",
+        negative_prompt="ugly, deformed, disfigured, bad anatomy, sharp, high quality, high detail",
+        generator=generator, num_inference_steps=30,
+    ).images[0]
+
+    image.save("text2img/blurry/30-steps/stained_glass%s.png" % randSeed)
+
+
+    ##50 inference blurry uncomment the preferred one
+    """
+    image = pipeline(
+        prompt="Astronaut in a jungle, cold color palette, muted colors, noisy, blurry, loss of sharpness",
+        negative_prompt="ugly, deformed, disfigured, bad anatomy, sharp, high quality, high detail",
+        generator=generator,
+    ).images[0]
+
+    image.save("text2img/blurry/50-steps/stained_glass%s.png" % randSeed)
+    """
+
+ ##30 inference steps foggy
+    """
+    image = pipeline(
+        prompt="a person on the street in fog, cold color palette, foggy, fog on the foreground, muted colors, blurry, loss of sharpness",
+        negative_prompt="ugly, deformed, disfigured, bad anatomy, clear, sharp, high quality, high detail",
+        generator=generator, num_inference_steps=30,
+    ).images[0]
+
+    image.save("text2img/foggy/30-steps/stained_glass%s.png" % randSeed)"""
+
+    """
+    ##50 inference foggy uncomment the preferred one
+    image = pipeline(
+        prompt="a person on the street in fog, cold color palette, foggy, fog on the foreground, muted colors, blurry, loss of sharpness",
+        negative_prompt="ugly, deformed, disfigured, bad anatomy, sharp, high quality, high detail",
+        generator=generator,
+    ).images[0]
+
+    image.save("text2img/foggy/50-steps/stained_glass%s.png" % randSeed)
+    """
+
+    i += 1
+
+
+
